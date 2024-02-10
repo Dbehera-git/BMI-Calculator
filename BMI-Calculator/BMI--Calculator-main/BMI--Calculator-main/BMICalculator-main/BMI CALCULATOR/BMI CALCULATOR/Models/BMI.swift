f9720cefb48d33e00eff52a246cd4884354dee32
Deepak Behera <136956471+Dbehera-git@users.noreply.github.com>
2024-02-10 20:24:41 +0530

    Add files via upload

---
+++ BMI-Calculator/BMI--Calculator-main/BMI--Calculator-main/BMICalculator-main/BMI CALCULATOR/BMI CALCULATOR/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI CALCULATOR
+//
+//  Created by Deepak Behera on 08/12/23.
+//
+
+import UIKit
+
+struct BMI {
+    
+    let value : Float
+    let adviceText : String
+    let colour : UIColor
+    
+}
